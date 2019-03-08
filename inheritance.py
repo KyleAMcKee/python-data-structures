9584240400ce4a68792e76ae92616d41b95870e5
Kyle Mckee <kyleandrewmckee@gmail.com>
2019-03-08 11:42:04 -0800

    simple script to learn inheritance

---
+++ inheritance.py
@@ -0,0 +1,63 @@
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@email.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+class Developer(Employee):
+    raise_amt = 1.12
+
+    def __init__(self, first, last, pay, prog_language):
+        super().__init__(first, last, pay)
+        self.prog_language = prog_language
+
+class Manager(Employee):
+    raise_amt = 1.1
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if not employees:
+            self.employees = []
+        else:
+            self.employees = employees
+    
+    def add_employee(self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+    
+    def remove_employee(self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+    
+    def print_employees(self):
+        for emp in self.employees:
+            print(emp.fullname())
+
+dev1 = Developer('Homer', 'Simpson', 50000, 'TypeScript')
+dev2 = Developer('Bart', 'Simpson', 65000, 'KabaScript')
+man1 = Manager('Marge', 'Simpson', 75000)
+
+man1.add_employee(dev1)
+man1.add_employee(dev2)
+man1.print_employees()
+man1.remove_employee(dev2)
+man1.print_employees()
+
+print(isinstance(man1, Developer))
+print(issubclass(Manager, Employee))
+
+
+# print(dev1.pay)
+# dev1.apply_raise()
+# print(dev1.pay)
+# print(dev2.pay)
